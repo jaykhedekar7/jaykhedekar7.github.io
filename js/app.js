2b74eecb739740a3d672004782eca242f2f55d26
jaykhedekar7 <jaykhedekar@hotmail.com>
2018-03-31 03:49:42 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -16,6 +16,22 @@ var x = setInterval(function () {
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+    if(days < 40 && days > 30){
+        document.getElementById("message").innerHTML = "Padhle, kuch ho sakta hai abhi";
+    }
+
+    else if(days <= 30 && days > 20){
+        document.getElementById("message").innerHTML = "Abhi toh padhle bhai!";
+    }
+
+    else if(days <= 20 && days > 10){
+        document.getElementById("message").innerHTML = "Ab nahi padha toh backlong pakka!";
+    }
+
+    else if(days <= 10 && days > 0){
+        document.getElementById("message").innerHTML = "Cheats bana sake utne hi din bache hai!";
+    }
+
     // Display the result in an element with id="demo"
     document.getElementById("demo").innerHTML = days + "d " + hours + "h " +
         minutes + "m " + seconds + "s ";
@@ -23,6 +39,6 @@ var x = setInterval(function () {
     // If the count down is finished, write some text
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("demo").innerHTML = "EXPIRED";
+        document.getElementById("demo").innerHTML = "JA BETA EXAM DE AAB!";
     }
 }, 1000);
